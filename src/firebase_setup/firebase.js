769fdbcb50f57707ee93fba4cee101d26d8ec1de
Folakunmi Aremu <fola.aremu@gmail.com>
2024-09-26 06:51:34 +0100

    update base firebase config

--- src/firebase_setup/firebase.js
+++ src/firebase_setup/firebase.js
@@ -7,14 +7,22 @@ import { getFirestore } from "@firebase/firestore"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     // apiKey: "AIzaSyDaWbc539oKFCQZSe7RRjeC7awgrRKb7KM",
+//     apiKey: "AIzaSyBmvBvFY2I7ilnUo5K6E6wzLykDBTv7IC0",
+//     // authDomain: "streampro-79790.firebaseapp.com",
+//     // projectId: "streampro-f7e7f",
+//     projectId: "streampro-768a8",
+//     // storageBucket: "streampro-79790.appspot.com",
+//     // messagingSenderId: "270100339480",
+//     appId: "1:412220407331:web:68c07d3b1386facb86bb96",
+//     // appId: "1:1058172241420:web:218c03fb44285b21ded029",
+//     // measurementId: "G-HS4ZLXXGMN"
+// };
 const firebaseConfig = {
-    apiKey: "AIzaSyDaWbc539oKFCQZSe7RRjeC7awgrRKb7KM",
-    // authDomain: "streampro-79790.firebaseapp.com",
-    projectId: "streampro-f7e7f",
-    // storageBucket: "streampro-79790.appspot.com",
-    // messagingSenderId: "270100339480",
-    appId: "1:1058172241420:web:218c03fb44285b21ded029",
-    // measurementId: "G-HS4ZLXXGMN"
+    apiKey: "AIzaSyBmvBvFY2I7ilnUo5K6E6wzLykDBTv7IC0",
+    projectId: "streampro-768a8",
+    appId: "1:412220407331:web:68c07d3b1386facb86bb96",
 };
 
 // Initialize Firebase
